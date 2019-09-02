e09bb073e71a19be682e56ae07030c857ff1b2c9
Davide Talon <davidetalon@MacBook-Pro-di-Davide-2.local>
2019-09-02 22:41:16 +0200

    removed batchnorm

--- src/model.py
+++ src/model.py
@@ -30,22 +30,18 @@ class Generative(nn.Module):
             # nn.ReLU(True),
     
             nn.ConvTranspose1d( nz, ngf * 8, 25, stride=4, padding=6, bias=True),
-            nn.BatchNorm1d(ngf * 8),
             nn.ReLU(True),
             # nn.Dropout(0.5),
             # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose1d(ngf * 8, ngf * 4, 25, 4, 6, bias=True),
-            nn.BatchNorm1d(ngf * 4),
             nn.ReLU(True),
             # nn.Dropout(0.5),
             # # state size. (ngf*4) x 8 x 8
             nn.ConvTranspose1d( ngf * 4, ngf * 2, 16, 4, 6, bias=True),
-            nn.BatchNorm1d(ngf * 2),
             nn.ReLU(True),
             # nn.Dropout(0.5),
             # state size. (ngf*2) x 16 x 16
             nn.ConvTranspose1d( ngf * 2, ngf, 25, 4, 6, bias=True),
-            nn.BatchNorm1d(ngf),
             nn.ReLU(True),
             # nn.Dropout(0.5),
 
@@ -125,15 +121,15 @@ def train_batch(gen, disc, batch, loss_fn, disc_optimizer, gen_optimizer, device
     disc_optimizer.zero_grad()
 
     # flipped labels and smoothing
-    real = torch.empty((batch_size,1), device=device).uniform_(0, 0.3)
-    fake = torch.empty((batch_size,1), device=device).uniform_(0.7, 1.2)
+    real = torch.empty((batch_size,1), device=device).uniform_(0, 0.1)
+    fake = torch.empty((batch_size,1), device=device).uniform_(0.9, 1.0)
 
     # noisy labels
-    noisy = torch.empty((batch_size,1), device=device).uniform_(0.7, 1.2)
+    noisy = torch.empty((batch_size,1), device=device).uniform_(0.9, 1.0)
     random = torch.rand(*real.shape, device=device)
     real = torch.where(random <= 0.05, noisy, real)
 
-    noisy = torch.empty((batch_size,1), device=device).uniform_(0, 0.3)
+    noisy = torch.empty((batch_size,1), device=device).uniform_(0, 0.1)
     random = torch.rand(*fake.shape, device=device)
     fake = torch.where(random <= 0.05, noisy, fake)
 
