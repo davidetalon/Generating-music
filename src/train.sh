739d91f56e239c893258c255d7943f93a1580957
Davide Talon <davidetalon@MacBook-Pro-di-Davide-2.local>
2019-09-05 10:08:57 +0200

    differentnoise=0.7/latentspace=1/ g_batchnorm/ngf=32/batch=16

--- src/train.sh
+++ src/train.sh
@@ -2,6 +2,6 @@ python src/train.py \
 --seed=30 \
 --gen_lr=0.0002 \
 --discr_lr=0.0002 \
---batch_size=32 \
+--batch_size=16 \
 --num_epochs=125 \
---notes='differentnoise=0.7/latentspace=1/ g_batchnorm/ngf=32/batch=32' \
+--notes='differentnoise=0.7/latentspace=1/ g_batchnorm/ngf=32/batch=16' \
